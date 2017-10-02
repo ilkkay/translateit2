9d653753545238aa09ff918a8a6b4f823106b410
ilkkay <iylakoski@gmail.com>
2017-10-02 11:32:24 +0300

    Simplified stremas in LanguageBeanCacheImpl

--- src/main/java/translateit2/languagebeancache/LanguageBeanCacheImpl.java
+++ src/main/java/translateit2/languagebeancache/LanguageBeanCacheImpl.java
@@ -1,5 +1,6 @@
 package translateit2.languagebeancache;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,13 +23,17 @@ public class LanguageBeanCacheImpl <F, T extends LanguageFile <F>>
 
     @Override
     public Optional<T> getService(F type) {
-        Map<F, T> services = serviceCache.entrySet().stream()
+    	// Optional.ofNullable() method returns a Non-empty Optional if a value present 
+    	// in the given object. Otherwise returns empty Optional.
+    	return Optional.ofNullable(serviceCache.get(type));
+    	
+        /* Map<F, T> services = serviceCache.entrySet().stream()
                 .filter(p -> p.getValue().getFileFormat().equals(type))
                 .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
         if (services.size() == 1)
             return Optional.of(services.get(type));
         else
-            return Optional.<T>empty();
+            return Optional.<T>empty(); */
     }
     
     @PostConstruct
@@ -38,9 +43,12 @@ public class LanguageBeanCacheImpl <F, T extends LanguageFile <F>>
     
     @Override
     public List<F> listFormatsSupported() {
-        List<F> formats = serviceCache.entrySet().stream().map(x -> x.getKey())
+        return new ArrayList<>(serviceCache.keySet());
+
+        /* List<F> formats = serviceCache.entrySet().stream().map(x -> x.getKey())
                 .collect(Collectors.toList());
-        return formats;
+        
+        return formats; */
     }
 
 }
